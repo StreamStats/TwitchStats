d314ed7fe25935bd5b85437aa29a7f2c26ebdfc8
Bacon_Space <thestreamstats@gmail.com>
2016-03-14 14:56:56 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function getData(){
          var avatar = data['logo'];
          var Username = data['display_name'];
          var followers = data['followers'];
-         var Follows = data['follows']['name'];
+         var Follows = data['follows'];
          var totalViews = data['views'];
          var sub = data['partner'];
          var mature = data['mature'];
