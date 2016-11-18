6fe76e4aeaa14f5be00a8e5c533b4c02f3cd5fd8
MRVDOG <me@mrvdog.com>
2016-11-18 20:49:14 +0000

    Fixed Script :)

--- js/main.js
+++ js/main.js
@@ -1,46 +1,37 @@
 $(document).ready(function(event){
-   $('#submit').click(function(event){
-      getData();
-      
-   });
-   $('#formThing').submit(function(event){
-      event.preventDefault();
-      getData();
+   $('#submit').click(getData);
+
+   $('#inputText').keyup(function(e){
+      if( e.which == 13 ) {
+          getData();
+      }
    });
 });
 function getData(){
-    $.get('https://api.twitch.tv/kraken/channels/' + $('#inputText').val() + '?client_id=ba5m54krq9n07wh8ra7ee5lb53vxaag'), function(data){
+     $.getJSON('https://api.twitch.tv/kraken/channels/' + $('#inputText').val() + '?client_id=ba5m54krq9n07wh8ra7ee5lb53vxaag', function(data){
          
-         var avatar = data['logo'];
-         var Username = data['display_name'];
-         var followers = data['followers'];
-         var totalViews = data['views'];
-         var sub = data['partner'];
-         var mature = data['mature'];
-                  if(data['mature'] != null){
-            var mature = data['mature'];
-         }else{
-            var mature = "None";
-         }
-         var online = data['status'];
-         if(data['game'] != null){
-            var lastPlayed = data['game'];
-         }else{
-            var lastPlayed = "None";
-         }
-         var joined = data['created_at'];
-         var html = '<center><img src="' + avatar + '"width="100px" height="100px" style="border:3px solid red">';
-         html += '<h1><span class="label label-success">' + Username + '</h1>';
-         html += '<br><b><span class="label label-success">Followers:' + followers + '</b>';
-         html += '<br><b><span class="label label-success">Total Views:' + totalViews + '</b>';
-         html += "<br>";
-         html += '<br><b><span class="label label-success">Current Status: ' + online + '</b>';
-         html += "<br>";
-         html += '<br><b><span class="label label-info"><font color="White"><a href="https://twitch.tv/' + Username + '">Twitch.tv/'+ Username +'</a></font></b>';
-         html += '<br><b><span class="label label-danger">Partnered:' + sub + '</b>';
-         html += '<br><b><span class="label label-primary">Has mature content: ' + mature + '</b>';
-         html += '<br><b><span class="label label-primary">Last Played: ' + lastPlayed + '</b>';
-         html += '<br><b><span class="label label-success">Joined on: '+joined+' </b>'.replace('T', ' at ');
+         var avatar = data.logo;
+            var Username = data.display_name;
+            var followers = data.followers;
+            var totalViews = data.views;
+            var sub = data.partner;
+            var online = data.status;
+            var joined = data.created_at.replace('T', ' at ');
+            var mature = (data.mature !== null) ? data.mature: "None";
+            var lastPlayed = (data.game !== null) ? data.game: "None";
+        
+         var html = '<center><img src="' + avatar + '"width="100px" height="100px" style="border:3px solid red">'+
+                     '<h1><span class="label label-success">' + Username + '</h1>'+
+                     '<br /><b><span class="label label-success">Followers:' + followers + '</b>'+
+                     '<br /><b><span class="label label-success">Total Views:' + totalViews + '</b>'+
+                     '<br />'+
+                     '<br /><b><span class="label label-success">Current Status: ' + online + '</b>'+
+                     '<br />'+
+                     '<br /><b><span class="label label-info"><font color="White"><a href="https://twitch.tv/' + Username + '">Twitch.tv/'+ Username +'</a></font></b>'+
+                     '<br /><b><span class="label label-danger">Partnered:' + sub + '</b>'+
+                     '<br /><b><span class="label label-primary">Has mature content: ' + mature + '</b>'+
+                     '<br /><b><span class="label label-primary">Last Played: ' + lastPlayed + '</b>'+
+                     '<br /><b><span class="label label-success">Joined on: '+joined+' </b>';
          $('.profile').html(html);
       }).fail(function(data){
             html = '<h1>A Twitch user with that name does not exist.';
